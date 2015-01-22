6bfccdae5ae515ce2981c7aaf65b1cfe8f9d92d3
Ray-Christian <cjray89@gmail.com>
2015-01-21 23:01:19 -0700

    Cleaned up the code, removed errors, added functionality. I created a basic scanner, input that will ask the user for their name, and then display a welcome message. I also added a constructor under the main class that calls the methods that were created so the methods actually happen. Running the program will allows you to enter your name in the console and it will give the welcome message back. Let me know what you think!

--- src/cit260/team4/battleship/CIT260Team4Battleship.java
+++ src/cit260/team4/battleship/CIT260Team4Battleship.java
@@ -5,19 +5,38 @@
  */
 package cit260.team4.battleship;
 
+import java.util.Scanner;
+
 /**
  *
  * @author erandall
  */
 public class CIT260Team4Battleship {
 
-    string firstName;//enter player first name
-       string lastName;//enter player last name     
+       
+       String firstName;//enter player first name
+       String description = "This is the battleship game. Enjoy!!";// a description of the game
+       
     public static void main(String[] args) {
-        public void getName(){}
-        public void displayHelp() {}
-        public void startGame(){}
-        public void playerMove(){}
+        CIT260Team4Battleship game = new CIT260Team4Battleship(); // constructor - creates instance of class caled "game"
+        game.getName(); //calls the getName() method for the "game" instance of the class
+        game.displayHelp(); //calls the getName() method for the "name" instance of the class
+    }
+        public void getName(){
+            Scanner input = new Scanner(System.in);   // gets input using scanner and puts it into variable firstName
+            System.out.println("What is your name?");
+            this.firstName = input.next();
+        }
+        public void displayHelp() {
+            System.out.println("Welcome, " + this.firstName ); //displays welcome message and description
+            System.out.println(this.description);
+        }
+        public void startGame(){
+            
+        }
+        public void playerMove(){
+            
+        }
     }
     
-}
+

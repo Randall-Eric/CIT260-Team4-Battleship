66b40e93c6c8e5be4382d81cbbb4cce717e75a27
Ray-Christian <cjray89@gmail.com>
2015-01-31 20:37:08 -0700

    added individual stuff

--- src/cit260/team4/battleship/Ships.java
+++ src/cit260/team4/battleship/Ships.java
@@ -21,4 +21,19 @@ public class Ships {
     public void showName(){
         System.out.println("The ship's name is: " + name);
     }
+    public void getAccuracy(int hits, int shots){
+        
+        double dShots = (double) shots;
+        double dHits = (double) hits;
+        double accuracy =0;
+        
+        if (shots == 0) 
+            System.out.println("\tThe Player\'s accuracy is Zero");
+        else if(shots < hits)
+            System.out.println("\tInvalid Input");
+        else {
+            accuracy = (dHits/dShots)*100;
+            System.out.println("\tThe Player\'s accuracy is " + accuracy + "%");
+        }
+    }
 }
